0e7266711574b2580809015d5a95cd75c71fb3c9
slam814 <abdussalam6111997@gmail.com>
2024-04-30 20:02:47 +0600

    this is my first commit

---
+++ types.js
@@ -0,0 +1,10 @@
+// strongly typed Language
+// int a = 5;
+
+
+const a = 5;
+const b = 'samsu kopai na ekhon ar';
+const ages = [45, 65, 48];
+const student = {id: 23, calass: 7};
+
+console.log(typeof a, typeof b, typeof ages, typeof student);
